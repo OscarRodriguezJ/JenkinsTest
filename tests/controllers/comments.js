74b3e4bc19228ef7134a75c6448aff27dcab56d8
Gerard Bruno Sanchez <gerard.bruno@quantion.com>
2016-03-15 17:39:53 +0100

    Update comments.js

--- tests/controllers/comments.js
+++ tests/controllers/comments.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function () {
@@ -8,14 +7,4 @@ describe('Array', function() {
     });
   });
 });
-=======
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
->>>>>>> c5cd042b8fb53405768ae5936a0674786751cf59
+
