d612c0546f6839ad770b3896288d4a462703f6de
gerardbrunoquantion <gerard.bruno@quantion.com>
2016-03-16 10:11:42 +0100

    otra prueba para automatizar

--- tests/controllers/comments.js
+++ tests/controllers/comments.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      console.log("Estoy dentro del te")
+      console.log("Estoy dentro del test")
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
